1172a06209c3ed47c2a1953584fd86b0f49bb2e4
kadernaz <76235766+kadernaz@users.noreply.github.com>
2020-12-20 02:39:57 -0600

    Update app.py
    Colors and storage /data

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome Update 5!"
+    #return 'Hello'
+    print(color)
+    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
 
-@app.route('/how are you updated')
-def hello():
-    return 'I am good, how about you?'
+@app.route('/color/<new_color>')
+def new_color(new_color):
+    return render_template('hello.html', name-socker.gethostname(), color=color_codes[new_color])
+
+@app.route('/read_file')
+def read_file():
+    f = open("data/testfile.txt")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(). contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
