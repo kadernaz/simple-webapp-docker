09d0873b3b34811d92e65710dc64c310d1cf8155
kadernaz <76235766+kadernaz@users.noreply.github.com>
2020-12-20 03:15:23 -0600

    Update app.py
    revert back to orig

--- app.py
+++ app.py
@@ -4,19 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    #return 'Hello'
-    print(color)
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[color])
-
-@app.route('/color/<new_color>')
-def new_color(new_color):
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
-
-@app.route('/read_file')
-def read_file():
-    f = open("data/testfile.txt")
-    contents = f.read()
-    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
+    return 'Welcome'
+    
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
